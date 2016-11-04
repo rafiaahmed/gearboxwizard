b31ca15039c469eab8022e24b982b5d3331ed7ed
rafiaahmed <raf4far@gmail.com>
2016-11-04 15:34:56 -0700

    trying to get dropdown value

--- js/calculator.js
+++ js/calculator.js
@@ -8,8 +8,6 @@ var requirements = document.querySelectorAll('#inputs ul') ;
 // Add onclick event to all the keys and perform operations
 for(var i = 0; i < keys.length; i++) {
 	
-	input.innerHTML = eval('inputs');
-	
 	keys[i].onclick = function(e) {
 		// Get the input and button values
 		var input = document.querySelector('.screen');
